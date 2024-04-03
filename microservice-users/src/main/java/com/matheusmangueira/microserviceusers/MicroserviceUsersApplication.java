5f1d297bb9d2f4c0a1de78430b5e4e415cc77771
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-04-03 14:59:22 -0300

    feat: create project for microservice-user

---
+++ microservice-users/src/main/java/com/matheusmangueira/microserviceusers/MicroserviceUsersApplication.java
@@ -0,0 +1,13 @@
+package com.matheusmangueira.microserviceusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceUsersApplication.class, args);
+	}
+
+}
---
+++ microservice-users/src/test/java/com/matheusmangueira/microserviceusers/MicroserviceUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusmangueira.microserviceusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
