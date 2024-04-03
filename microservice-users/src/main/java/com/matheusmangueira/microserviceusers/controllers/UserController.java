58a1a23601a542016122c5d02599975f8b7016a7
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-04-03 14:58:03 -0300

    feat: create controller for User

---
+++ microservice-users/src/main/java/com/matheusmangueira/microserviceusers/controllers/UserController.java
@@ -0,0 +1,49 @@
+package com.matheusmangueira.microserviceusers.controllers;
+
+import com.matheusmangueira.microserviceusers.domain.User;
+import com.matheusmangueira.microserviceusers.dtos.UserDTO;
+import com.matheusmangueira.microserviceusers.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/users")
+@RestController
+public class UserController {
+
+  @Autowired
+  private UserService userService;
+
+  @GetMapping("/all")
+  public ResponseEntity<List<User>> getAllUsers(@RequestParam(defaultValue = "1")
+  Integer page, @RequestParam(defaultValue = "10") Integer limit){
+
+    Pageable pageable = PageRequest.of(page - 1, limit);
+    Page<User> usersPage = userService.getAllUsers(pageable);
+    List<User> users = usersPage.getContent();
+
+    return ResponseEntity.status(HttpStatus.OK).body(users);
+  }
+
+  @GetMapping("/id")
+  public ResponseEntity<User> getUserById(@RequestParam String id){
+
+    User user = userService.getUserById(id);
+
+    return ResponseEntity.status(HttpStatus.OK).body(user);
+  }
+
+  @PostMapping
+  public ResponseEntity<User> createUser(@RequestBody UserDTO user){
+
+    User newUser = userService.createUser(user);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+  }
+}
