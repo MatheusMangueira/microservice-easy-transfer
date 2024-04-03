87b8feaa1b417fd77a898f18bf668eec775756e6
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-04-03 14:58:24 -0300

    feat: create Repository for User

---
+++ microservice-users/src/main/java/com/matheusmangueira/microserviceusers/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.matheusmangueira.microserviceusers.repositories;
+
+import com.matheusmangueira.microserviceusers.domain.User;
+import jakarta.annotation.Nonnull;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+  @Nonnull
+  Optional<User> findById(@Nonnull String id);
+}
+
