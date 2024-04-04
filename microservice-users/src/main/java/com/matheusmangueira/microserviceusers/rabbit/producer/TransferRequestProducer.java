f22e246c5705b218d1979ef9da252466101499f3
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-04-04 19:01:37 -0300

    feat: create connecting the User with rabbitMq / creating a queue for transfer requests

---
+++ microservice-users/src/main/java/com/matheusmangueira/microserviceusers/rabbit/producer/TransferRequestProducer.java
@@ -0,0 +1,51 @@
+package com.matheusmangueira.microserviceusers.rabbit.producer;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.amqp.core.*;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransferRequestProducer {
+  private static final String NAME_EXCHANGE = "amq.direct";
+
+  private AmqpAdmin amqpAdmin;
+
+  public TransferRequestProducer(AmqpAdmin amqpAdmin) {
+    this.amqpAdmin = amqpAdmin;
+  }
+
+  private Queue row(String rowName) {
+    return new Queue(rowName, true, false, false);
+  }
+
+  private DirectExchange exchange() {
+    return new DirectExchange(NAME_EXCHANGE);
+  }
+
+  private Binding bind(Queue row, DirectExchange exchange) {
+    return new Binding(
+        row.getName(),
+        Binding.DestinationType.QUEUE,
+        exchange.getName(),
+        row.getName(),
+        null);
+  }
+
+  @PostConstruct
+  public void send() {
+    Queue row = row("transfer-row");
+
+    DirectExchange exchange = exchange();
+
+    Binding relation = bind(row, exchange);
+
+    //criando fila no RabbitMQ
+    this.amqpAdmin.declareQueue(row);
+    //criando exchange no RabbitMQ
+    this.amqpAdmin.declareExchange(exchange);
+    //criando relação fila-exchange no RabbitMQ
+    this.amqpAdmin.declareBinding(relation);
+  }
+
+
+}
