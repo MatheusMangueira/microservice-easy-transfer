0c2c44c068a3ce89700545ce30a1595818c3b4e3
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-04-04 21:15:27 -0300

    feat: creating new transfer microservice

---
+++ microservice-transfer/src/main/java/com/matheusmangueira/microserviceTransfer/MicroserviceTransferApplication.java
@@ -0,0 +1,13 @@
+package com.matheusmangueira.microserviceTransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceTransferApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MicroserviceTransferApplication.class, args);
+  }
+
+}
---
+++ microservice-transfer/src/test/java/com/matheusmangueira/microserviceTransfer/MicroserviceTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusmangueira.microserviceTransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
