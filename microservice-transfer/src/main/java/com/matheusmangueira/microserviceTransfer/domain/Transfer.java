d0a100f7caf9b1f4051244e06a34ee5e40b23469
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-04-05 02:31:58 -0300

    feat: entity for transfer

---
+++ microservice-transfer/src/main/java/com/matheusmangueira/microserviceTransfer/domain/Transfer.java
@@ -0,0 +1,42 @@
+package com.matheusmangueira.microserviceTransfer.domain;
+
+import dtos.TransferRequestDTO;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "transfer")
+@Table(name = "transfer_entity")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transfer {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private String id;
+
+  @Column(nullable = false)
+  @NotBlank
+  private String senderID;
+
+  @Column(nullable = false)
+  @NotBlank
+  private String receiverID;
+
+  @Column(nullable = false)
+  @NotNull
+  private BigDecimal value;
+
+  public Transfer(TransferRequestDTO TransferRequestDTO ) {
+    this.senderID = TransferRequestDTO.senderID;
+    this.receiverID = TransferRequestDTO.recipientID;
+    this.value = TransferRequestDTO.value;
+  }
+
+}
