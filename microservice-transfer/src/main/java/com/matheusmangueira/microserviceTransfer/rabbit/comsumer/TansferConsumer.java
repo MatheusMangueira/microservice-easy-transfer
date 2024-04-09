20cacd6086114d7729ca75e7f220240f9fd5e2f2
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-04-09 09:42:13 -0300

    feat: transferConsumer adding transfer method

--- microservice-transfer/src/main/java/com/matheusmangueira/microserviceTransfer/rabbit/comsumer/TansferConsumer.java
+++ microservice-transfer/src/main/java/com/matheusmangueira/microserviceTransfer/rabbit/comsumer/TansferConsumer.java
@@ -2,32 +2,36 @@ package com.matheusmangueira.microserviceTransfer.rabbit.comsumer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.matheusmangueira.microserviceTransfer.services.TransferService;
 import dtos.TransferRequestDTO;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 
 import java.math.BigDecimal;
 
 @Component
 public class TansferConsumer {
+  @Autowired
+  private TransferService transferService;
 
   @RabbitListener(queues = "transfer-row")
   public void consumer(String message) throws JsonProcessingException, InterruptedException {
     TransferRequestDTO transferRequestDTO = new ObjectMapper().readValue(message, TransferRequestDTO.class);
 
-    BigDecimal subtract = transferRequestDTO.senderID.balance()
-        .subtract(transferRequestDTO.value);
+    transferService.transferAccount(
+        transferRequestDTO.value,
+        transferRequestDTO.senderID,
+        transferRequestDTO.recipientID
+    );
 
-    BigDecimal add = transferRequestDTO.recipientID.balance()
-        .add(transferRequestDTO.value);
 
     System.out.println("Transfer: " + message);
-    System.out.println("Sender: " + transferRequestDTO.senderID.balance());
-    System.out.println("subtract Sender: " + subtract);
-    System.out.println("Recipient: " + transferRequestDTO.recipientID.balance());
-    System.out.println("add Recipient: " + add);
-    System.out.println("Value: " + transferRequestDTO.senderID.name());
-    System.out.println("Value: " + transferRequestDTO.recipientID.name());
+    System.out.println("Sender: " + transferRequestDTO.senderID.balance);
+    System.out.println("Recipient: " + transferRequestDTO.recipientID.balance);
+    System.out.println("Value: " + transferRequestDTO.senderID.name);
+    System.out.println("Value: " + transferRequestDTO.recipientID.name);
     System.out.println("-------------------------");
   }
 }
