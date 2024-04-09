fdc0ee343b311d0f03542c3d3e9f651aaeaef104
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-04-09 09:43:25 -0300

    feat: add transfer Option for findById

--- microservice-transfer/src/main/java/com/matheusmangueira/microserviceTransfer/repositories/TransferRepository.java
+++ microservice-transfer/src/main/java/com/matheusmangueira/microserviceTransfer/repositories/TransferRepository.java
@@ -4,6 +4,11 @@ import com.matheusmangueira.microserviceTransfer.domain.Transfer;
 import jakarta.annotation.Nonnull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TransferRepository extends JpaRepository<Transfer, String> {
+
+  @Nonnull
+  Optional<Transfer> findById(@Nonnull String id);
 
 }
