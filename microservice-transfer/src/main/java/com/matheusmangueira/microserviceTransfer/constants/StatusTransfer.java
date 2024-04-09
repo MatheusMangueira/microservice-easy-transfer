43197b4d5eb67f5e8770e4e0d55fd96ff35df6de
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-04-09 09:45:22 -0300

    feat: create constants for notification

---
+++ microservice-transfer/src/main/java/com/matheusmangueira/microserviceTransfer/constants/StatusTransfer.java
@@ -0,0 +1,6 @@
+package com.matheusmangueira.microserviceTransfer.constants;
+
+public class StatusTransfer {
+  public static final String COMPLETED = "COMPLETED";
+  public static final String CANCELED = "CANCELED";
+}
