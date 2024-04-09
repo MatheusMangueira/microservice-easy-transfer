4ed6ccf28ac2324e198e5cca539431c3aebebd06
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-04-09 14:50:49 -0300

    feat: create constants for notification

---
+++ microservice-users/src/main/java/com/matheusmangueira/microserviceusers/constants/UserConstants.java
@@ -0,0 +1,6 @@
+package com.matheusmangueira.microserviceusers.constants;
+
+public class UserConstants {
+  public static final String USER_COMPLETED = "transição feita com sucesso";
+  public static final String USER_CANCELED = "transição cancelada";
+}
