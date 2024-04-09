e2fcee25896dc90b2ffd83668c2e98cdb4ca521e
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-04-09 13:28:00 -0300

    fix: implements Serializable user DTO

--- librabbitmq/src/main/java/dtos/UserDTO.java
+++ librabbitmq/src/main/java/dtos/UserDTO.java
@@ -1,10 +1,11 @@
 package dtos;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Optional;
 import java.util.UUID;
 
-public class UserDTO {
+public class UserDTO implements Serializable {
   public String id;
   public String name;
   public String email;
